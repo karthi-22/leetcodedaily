fe7fa2c3ee2e1ca7417c76d76b49b9ed1a00028e
i_karthikeyan.S <129253948+karthi-22@users.noreply.github.com>
2023-09-05 10:12:54 +0530

    Time: 37 ms (92.62%) | Memory: 17.3 MB (70.41%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -1,43 +1,36 @@
-class Solution(object):
-    def copyRandomList(self, head):
-        """
-        :type head: Node
-        :rtype: Node
-        """
-        if not head:
-            return head
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
 
-        # Creating a new weaved list of original and copied nodes.
-        ptr = head
-        while ptr:
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
 
-            # Cloned node
-            new_node = Node(ptr.val, None, None)
-
-            # Inserting the cloned node just next to the original node.
-            # If A->B->C is the original linked list,
-            # Linked list after weaving cloned nodes would be A->A'->B->B'->C->C'
-            new_node.next = ptr.next
-            ptr.next = new_node
-            ptr = new_node.next
-
-        ptr = head
-
-        # Now link the random pointers of the new nodes created.
-        # Iterate the newly created list and use the original nodes random pointers,
-        # to assign references to random pointers for cloned nodes.
-        while ptr:
-            ptr.next.random = ptr.random.next if ptr.random else None
-            ptr = ptr.next.next
-
-        # Unweave the linked list to get back the original linked list and the cloned list.
-        # i.e. A->A'->B->B'->C->C' would be broken to A->B->C and A'->B'->C'
-        ptr_old_list = head # A->B->C
-        ptr_new_list = head.next # A'->B'->C'
-        head_new = head.next
-        while ptr_old_list:
-            ptr_old_list.next = ptr_old_list.next.next
-            ptr_new_list.next = ptr_new_list.next.next if ptr_new_list.next else None
-            ptr_old_list = ptr_old_list.next
-            ptr_new_list = ptr_new_list.next
-        return head_new
+        dic = {None:None}
+        curr = head
+        while curr:
+            dic[curr ] = Node(curr.val)
+            curr = curr.next
+        curr = head
+        while curr:
+            copy = dic[curr]
+            copy.next = dic[curr.next]
+            copy.random = dic[curr.random]
+            curr = curr.next
+        return dic[head]
+        dic={None:None}
+        cur=head
+        while cur:
+            dic[cur]=Node(cur.val)
+            cur=cur.next
+        cur=head
+        while cur:
+            copy=dic[cur]
+            copy.next=dic[cur.next]
+            copy.random=dic[cur.random]
+            cur=cur.next
+        return dic[head]
